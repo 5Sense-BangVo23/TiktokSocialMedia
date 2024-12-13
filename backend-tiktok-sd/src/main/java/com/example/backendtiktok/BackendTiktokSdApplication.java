c12ee532b3447edec02bb7b05d1c5f47057e5e22
bang-dev <134923994+5Sense-BangVo23@users.noreply.github.com>
2024-12-14 01:26:03 +0700

    Setup

---
+++ backend-tiktok-sd/src/main/java/com/example/backendtiktok/BackendTiktokSdApplication.java
@@ -0,0 +1,13 @@
+package com.example.backendtiktok;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackendTiktokSdApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BackendTiktokSdApplication.class, args);
+	}
+
+}
---
+++ backend-tiktok-sd/src/test/java/com/example/backendtiktok/BackendTiktokSdApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.backendtiktok;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendTiktokSdApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
